fca1d9a942f2504808821605bd922a789c559c3b
halaabushehab <hala.abushiihab@gmail.com>
2025-01-16 21:33:05 +0300

    edit html pags

---
+++ userProfile.js
@@ -0,0 +1,14 @@
+  // hover logo navbar
+  const logo = document.querySelector('.logo');
+        const icon = document.querySelector('.icon');
+
+        logo.addEventListener('mouseover', () => {
+            icon.style.color = '#EECAD5';
+        });
+
+        logo.addEventListener('mouseout', () => {
+            icon.style.color = '#FF69B4';
+        });
+
+
+//main ======================
